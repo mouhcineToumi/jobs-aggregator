5e0e705fa954bc00827ee06037e06b35d6097508
Ait moumen <youssef.aitmoumen@gmail.com>
2020-06-21 02:04:08 +0100

    Header + CardList

--- front/app/containers/HomePage/index.js
+++ front/app/containers/HomePage/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable no-unused-vars */
 /*
  * HomePage
  *
@@ -9,16 +11,48 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import Header from 'components/Header';
 import CardJobs from 'components/CardJobs';
+import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import Divider from '@material-ui/core/Divider';
+import { makeStyles } from '@material-ui/core/styles';
+
 import messages from './messages';
 
+const useStyles = makeStyles(theme => ({
+  cont: {
+    marginTop: '20px',
+  },
+  beet: {
+    display: 'grid',
+    gridTemplateRows: '1fr 1fr',
+    margin: '30px',
+    alignItems: 'center',
+  },
+  text: {
+    fontFamily: 'Andika, sans-serif',
+    fontWeight: 'bold',
+    fontSize: '17px',
+    color: 'rgba(0, 0, 0, 0.38)',
+  },
+}));
 export default function HomePage() {
+  const classes = useStyles();
   return (
-    <div>
-      <Container maxWidth="sm">
+    <>
+      <Container className={classes.cont} maxWidth="md">
         <Header />
+        <div className={classes.beet}>
+          <Typography
+            align="center"
+            color="textSecondary"
+            className={classes.text}
+          >
+            <span>1000 Jobs </span>
+          </Typography>
+          <Divider />
+        </div>
         <CardJobs />
       </Container>
-    </div>
+    </>
   );
 }
